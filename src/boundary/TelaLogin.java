fc037538d7086768d23fdf2e074196be5d50a13d
ExCal <roberto.mitsunari@fatec.sp.gov.br>
2020-10-31 18:52:12 -0300

    Tela login v1

--- src/boundary/TelaLogin.java
+++ src/boundary/TelaLogin.java
@@ -1,10 +1,50 @@
 package boundary;
 
-public class TelaLogin {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class TelaLogin extends Application{
+
+	@Override
+	public void start(Stage stage) { 
+		Pane painel = new Pane();
+		Scene scn = new Scene(painel, 400, 200);
+		
+		Label lblUser = new Label("usuário: ");
+		lblUser.relocate(20, 10);
+		TextField txtUser = new TextField();
+		txtUser.relocate(100, 10);
+		txtUser.setPrefColumnCount(15);
+		
+		Label lblSenha = new Label("senha: ");
+		lblSenha.relocate(20, 40);
+		TextField txtSenha = new TextField();
+		txtSenha.relocate(100, 40);
+		txtSenha.setPrefColumnCount(15);
+		
+		Button btnEntrar = new Button("Entrar");
+		btnEntrar.relocate(100, 80);
+		
+		Label txtEsqueci = new Label("Esqueci a senha?");
+		txtEsqueci.relocate(180, 80);
+
+		painel.getChildren().addAll(
+				lblUser, txtUser, 
+				lblSenha,  txtSenha,
+				btnEntrar, txtEsqueci);
+
+		stage.setScene(scn);
+		stage.setTitle("Tela Login");
+		stage.show();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Application.launch(TelaLogin.class, args);
 	}
 
 }
