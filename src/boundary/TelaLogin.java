73e3140addd1d7ff867c4f1f6f58465c69e75013
AdrianoGalan <dricogalan@gmail.com>
2020-10-31 23:04:50 -0300

    .

--- src/boundary/TelaLogin.java
+++ src/boundary/TelaLogin.java
@@ -9,35 +9,32 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class TelaLogin extends Application{
+public class TelaLogin extends Application {
 
 	@Override
-	public void start(Stage stage) { 
+	public void start(Stage stage) {
 		Pane painel = new Pane();
 		Scene scn = new Scene(painel, 300, 140);
 		stage.setResizable(false);
-		Label lblUser = new Label("usuário: ");
+		Label lblUser = new Label("usuï¿½rio: ");
 		lblUser.relocate(20, 14);
 		TextField txtUser = new TextField();
 		txtUser.relocate(100, 10);
 		txtUser.setPrefColumnCount(15);
-		
+
 		Label lblSenha = new Label("senha: ");
 		lblSenha.relocate(20, 43);
 		TextField txtSenha = new TextField();
 		txtSenha.relocate(100, 40);
 		txtSenha.setPrefColumnCount(15);
-		
+
 		Button btnEntrar = new Button("Entrar");
 		btnEntrar.relocate(100, 80);
 		Hyperlink hplEsqueci = new Hyperlink("Esqueci a senha?");
 
 		hplEsqueci.relocate(180, 80);
 
-		painel.getChildren().addAll(
-				lblUser, txtUser, 
-				lblSenha,  txtSenha,
-				btnEntrar, hplEsqueci);
+		painel.getChildren().addAll(lblUser, txtUser, lblSenha, txtSenha, btnEntrar, hplEsqueci);
 
 		stage.setScene(scn);
 		stage.setTitle("Tela Login");
